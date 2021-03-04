8f2425eae96d9399db0e631377f92c2bab264d23
Keji Li <kli@acadian-asset.com>
2021-03-04 15:38:19 -0500

    remove lsp diagnostics in quickfix

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -19,24 +19,6 @@ vim.fn.sign_define( "LspDiagnosticsSignWarning", { text = "", texthl = "LspDi
 vim.fn.sign_define( "LspDiagnosticsSignInformation", { text = "", texthl = "LspDiagnosticsSignInformation" })
 vim.fn.sign_define("LspDiagnosticsSignHint", {text = "➤", texthl = "LspDiagnosticsSignHint"})
 
---- move diagnostics to quickfix ---
-do
-  local method = "textDocument/publishDiagnostics"
-  local default_callback = vim.lsp.callbacks[method]
-  vim.lsp.callbacks[method] = function(err, method, result, client_id)
-    default_callback(err, method, result, client_id)
-    if result and result.diagnostics then
-      for _, v in ipairs(result.diagnostics) do
-        v.bufnr = client_id
-        v.lnum = v.range.start.line + 1
-        v.col = v.range.start.character + 1
-        v.text = v.message
-      end
-      vim.lsp.util.set_qflist(result.diagnostics)
-    end
-  end
-end
-
 --- pyright ---
 configs.pyright = {
     default_config = {
