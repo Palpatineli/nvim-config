d08afa795272e70a8f0e1c20bc1b0b10cf334975
Keji Li <kli@acadian-asset.com>
2021-08-16 01:04:05 -0400

    converted vimcmdline setting to lua

---
+++ ftplugin/python.lua
@@ -0,0 +1,15 @@
+-- General
+vim.wo.foldnestmax=2
+
+vim.api.nvim_buf_set_keymap(0, "n", "<F5>", ":w<cr>:!python %<cr>", {noremap = true})
+vim.api.nvim_buf_set_keymap(0, "i", "<F6>", "import pdb; pdb.set_trace()", {noremap = true})
+
+-- vimcmdline
+vim.cmd [[
+packadd vim-python-pep8-indent
+packadd vimcmdline
+]]
+vim.api.nvim_buf_set_keymap(0, "n", "<F7>", "<cmd>call VimCmdLineStartApp()<cr>", {noremap = true, silent = true})
+vim.api.nvim_buf_set_keymap(0, "n", "<F9>", "<cmd>call VimCmdLineQuit('python')<cr>", {noremap = true, silent = true})
+vim.api.nvim_buf_set_keymap(0, "n", "<leader>R", "?^##<cr>jV/^##<cr>k<esc>:call VimCmdLineSendSelection()<cr>jj:nohl<cr>", {noremap = true, silent = true})
+vim.api.nvim_buf_set_keymap(0, "v", "<leader>R", "<esc>:call VimCmdLineSendSelection()<cr>", {noremap = true, silent = true})
