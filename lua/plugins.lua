b73c5e6b49292f433df8c5b65fff4f226d1ed195
Keji Li <kli@acadian-asset.com>
2022-07-20 09:59:33 -0400

    fix: ufo virtual text fixed
    by moving handler inside of setup and fixing catppuccin compile

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -320,47 +320,59 @@ local setup_lightspeed = function()
     require'lightspeed'.setup({})
 end
 
-local handler = function(virtText, lnum, endLnum, width, truncate)
-    local newVirtText = {}
-    local suffix = ('  %d '):format(endLnum - lnum)
-    local sufWidth = vim.fn.strdisplaywidth(suffix)
-    local targetWidth = width - sufWidth
-    local curWidth = 0
-    for _, chunk in ipairs(virtText) do
-        local chunkText = chunk[1]
-        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-        if targetWidth > curWidth + chunkWidth then
-            table.insert(newVirtText, chunk)
-        else
-            chunkText = truncate(chunkText, targetWidth - curWidth)
-            local hlGroup = chunk[2]
-            table.insert(newVirtText, {chunkText, hlGroup})
-            chunkWidth = vim.fn.strdisplaywidth(chunkText)
-            -- str width returned from truncate() may less than 2nd argument, need padding
-            if curWidth + chunkWidth < targetWidth then
-                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-            end
-            break
-        end
-        curWidth = curWidth + chunkWidth
-    end
-    table.insert(newVirtText, {suffix, 'MoreMsg'})
-    return newVirtText
-end
-
 local setup_ufo = function()
-    vim.o.foldcolumn = '0'
+    vim.o.foldcolumn = '2'
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = -1
     vim.o.foldenable = true
     local ufo = require'ufo'
     vim.keymap.set('n', 'zR', ufo.openAllFolds)
     vim.keymap.set('n', 'zM', ufo.closeAllFolds)
+    local handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = ('  %d '):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+            local chunkText = chunk[1]
+            local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            if targetWidth > curWidth + chunkWidth then
+                table.insert(newVirtText, chunk)
+            else
+                chunkText = truncate(chunkText, targetWidth - curWidth)
+                local hlGroup = chunk[2]
+                table.insert(newVirtText, {chunkText, hlGroup})
+                chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                -- str width returned from truncate() may less than 2nd argument, need padding
+                if curWidth + chunkWidth < targetWidth then
+                    suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+                end
+                break
+            end
+            curWidth = curWidth + chunkWidth
+        end
+        table.insert(newVirtText, {suffix, 'MoreMsg'})
+        return newVirtText
+    end
     ufo.setup({
         provider_selector = function(bufnr, filetype, buftype) return {'treesitter', 'indent'} end,
-        fold_virt_text_handler = handler,
-        enable_fold_end_virt_text = true
+        fold_virt_text_handler = handler
     })
+end
+
+local setup_catppuccin = function()
+    local catppuccin = require'catppuccin'
+    catppuccin.setup({
+        dim_inactive = {enabled = true},
+        integrations = {
+            lightspeed = true,
+            ts_rainbow = true
+        },
+        compile = {enabled = true}
+    })
+    vim.g.catppuccin_flavour = 'frappe'
+    vim.cmd[[colorscheme catppuccin]]
 end
 
 require('packer').startup(function(use)
@@ -399,19 +411,7 @@ require('packer').startup(function(use)
     use 'neovim/nvim-lspconfig'
     use {'onsails/lspkind-nvim', requires={'hrsh7th/nvim-cmp'}}
     use {'euclio/vim-markdown-composer', run='cargo build --release', opt={'markdown'}}
-    use {"catppuccin/nvim", as="catppuccin",
-         config=function()
-            local catppuccin = require'catppuccin'
-            catppuccin.setup({
-                integrations = {
-                    lightspeed = true,
-                    ts_rainbow = true
-                }
-            })
-            vim.g.catppuccin_flavour = 'frappe'
-            vim.cmd[[colorscheme catppuccin]]
-        end
-    }
+    use {"catppuccin/nvim", as="catppuccin", config=setup_catppuccin }
     use {'jbyuki/nabla.nvim', ft={'markdown'}}
     use 'nvim-lua/plenary.nvim'
     use 'nvim-neorg/neorg-telescope'
