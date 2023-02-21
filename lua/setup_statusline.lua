d93eded6858731ef196240c0c7aba374309e79fb
Keji Li <kli@acadian-asset.com>
2023-02-21 14:35:47 -0500

    feat: powerline statusline coloring

--- lua/setup_statusline.lua
+++ lua/setup_statusline.lua
@@ -97,6 +97,90 @@ M.lualine = function(theme)
     })
 end
 
+M.lualine_power = function(theme)
+    local custom_theme = require('lualine.themes.'..theme)
+    local empty = require('lualine.component'):extend()
+    function empty:draw(default_highlight)
+      self.status = ''
+      self.applied_separator = ''
+      self:apply_highlights(default_highlight)
+      self:apply_section_separators()
+      return self.status
+    end
+
+    local function process_sections(sections)
+      for name, section in pairs(sections) do
+        local left = name:sub(9, 10) < 'x'
+        for pos = 1, name ~= 'lualine_z' and #section or #section - 1 do
+          table.insert(section, pos * 2, { empty, color = { fg = custom_theme.normal.a.fg,
+                    bg = custom_theme.normal.a.fg } })
+        end
+        for id, comp in ipairs(section) do
+          if type(comp) ~= 'table' then
+            comp = { comp }
+            section[id] = comp
+          end
+          comp.separator = left and { right = '' } or { left = '' }
+        end
+      end
+      return sections
+    end
+
+    require('lualine').setup {
+      options = {
+        theme = theme,
+        component_separators = '',
+        section_separators = { left = '', right = '' },
+      },
+      sections = process_sections {
+        lualine_a = { 'mode' },
+        lualine_b = {
+          'branch',
+          'diff',
+          {
+            'diagnostics',
+            source = { 'nvim' },
+            sections = { 'error' },
+            diagnostics_color = { error = { bg = custom_theme.visual.a.bg, fg = custom_theme.normal.a.fg } },
+          },
+          {
+            'diagnostics',
+            source = { 'nvim' },
+            sections = { 'warn' },
+            diagnostics_color = { warn = { bg = custom_theme.replace.a.bg, fg = custom_theme.normal.a.fg } },
+          },
+          { 'filename', file_status = false, path = 1 },
+          {
+            '%w',
+            cond = function()
+              return vim.wo.previewwindow
+            end,
+          },
+          {
+            '%r',
+            cond = function()
+              return vim.bo.readonly
+            end,
+          },
+          {
+            '%q',
+            cond = function()
+              return vim.bo.buftype == 'quickfix'
+            end,
+          },
+        },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = { {'filetype', color={bg=custom_theme.normal.b.bg}} },
+        lualine_z = { {'%l:%c', color={bg = custom_theme.command.a.bg} }, '%p%%/%L' },
+      },
+      inactive_sections = {
+        lualine_c = { '%f %y %m' },
+        lualine_x = {},
+      },
+    }
+end
+
 M.miniline = function()
     local gps = setup_gps()
     local mini = require'mini.statusline'
