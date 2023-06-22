561b39ff90b4b6ca6c97cc08693b343fc49cf2eb
Keji Li <kli@acadian-asset.com>
2023-06-22 17:14:38 -0400

    feat: move cmp setup to setup_lsp

--- lua/setup_lsp.lua
+++ lua/setup_lsp.lua
@@ -1,3 +1,62 @@
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local setup_cmp = function()
+    local cmp = require'cmp'
+    local luasnip = require'luasnip'
+    cmp.setup({
+        enabled = function ()
+            return vim.api.nvim_buf_get_option(0, 'buftype') ~= 'prompt' or require'cmp_dap'.is_dap_buffer()
+        end,
+        snippet = {
+            expand = function(args) luasnip.lsp_expand(args.body) end,
+        },
+        mapping = {
+            ['<C-Space>'] = cmp.mapping.complete({}),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ['<C-j>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif luasnip.expand_or_jumpable() then
+                    luasnip.expand_or_jump()
+                elseif has_words_before() then
+                    cmp.complete()
+                else
+                    fallback()
+                end
+            end, { "i", "s", "c" }),
+            ['<C-k>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                else
+                    fallback()
+                end
+            end, { "i", "s", "c" }),
+        },
+        sources = {
+            { name = 'luasnip' },
+            { name = 'nvim_lsp' },
+            { name = 'nvim_lsp_signature_help' },
+            { name = 'dap' },
+            { name = 'path' },
+            { name = 'treesitter' },
+            { name = 'buffer', keyword_length = 4},
+        }
+    })
+    cmp.setup.cmdline('/', {
+        sources = cmp.config.sources({
+            { name = 'nvim_lsp_document_symbol' }
+        },
+        {
+            { name = 'buffer' }
+        })
+    })
+end
+
 local M = {}
 M.setup = function()
     local navic = require'nvim-navic'
@@ -78,5 +137,6 @@ M.setup = function()
             },
         },
     }
+    setup_cmp()
 end
 return M
