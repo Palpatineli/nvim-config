74a27436dbd4bb0ad7e64c669398ea6ec74b8560
Keji Li <mail@keji.li>
2026-02-09 10:24:26 -0500

    fix: better copypaste in powershell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -34,20 +34,18 @@ vim.opt.clipboard = 'unnamedplus'
 vim.opt.updatetime = 500
 
 if vim.fn.has('wsl') == 1 then
-    if vim.fn.executable('win32yank.exe') == 1 then
-        vim.g.clipboard = {
-            name = 'win32yank',
-            copy = {
-                ["+"] = 'win32yank.exe -i --crlf',
-                ["*"] = 'win32yank.exe -i --crlf',
-            },
-            paste = {
-                ["+"] = 'win32yank.exe -o --lf',
-                ["*"] = 'win32yank.exe -o --lf',
-            },
-            cache_enabled = 0,
-        }
-    end
+    vim.g.clipboard = {
+        name = 'win32yank',
+        copy = {
+            ["+"] = 'clip.exe',
+            ["*"] = 'clip.exe',
+        },
+        paste = {
+            ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
 end
 
 -- filetypes
