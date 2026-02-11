f30069af7ec19eaa30f297198cb76901f4bf1d6c
Keji Li <mail@keji.li>
2026-02-10 19:08:27 -0500

    switch back from python repl to iron

--- lua/plugins/python-repl.lua
+++ lua/plugins/python-repl.lua
@@ -1,12 +1,31 @@
 return {
-    {"geg2102/nvim-python-repl",
+    {"Vigemus/iron.nvim",
     ft = {"python", "lua", "scala"}, 
     config = function()
-        require("nvim-python-repl").setup({
-            execute_on_send = true,
-            vsplit = false,
-        })
-        vim.keymap.set("v", ";<space>", function() require('nvim-python-repl').send_visual_to_repl() end, { desc = "Send visual selection to REPL"})
-        vim.api.nvim_set_keymap("n", ";<space>", "?^# %%<cr>jV/^# %%<cr>k<esc>:lua require('nvim-python-repl').send_visual_to_repl()<cr>jj:nohl<cr>", { desc = "Send section to REPL", noremap = true})
+        local iron = require'iron.core'
+        local view = require'iron.view'
+        local common = require'iron.fts.common'
+        iron.setup{
+            config={
+                scratch_repl = true,
+                repl_definition = {
+                    python = {
+                        command = {'ipython', '--no-autoindent'},
+                        format = common.bracketed_paste_python,
+                        block_dividers = { "# %%", "#%%" },
+                        env = {PYTHON_BASIC_REPL = "1"} --this is needed for python3.13 and up.
+                    }
+                },
+                repl_filetype = function(bufnr, ft) return ft end,
+                dap_integration = true,
+                repl_open_cmd = view.bottom(40),
+            },
+            keymaps = {
+                visual_send = ";<space>",
+                send_code_block_and_move = ";<space>"
+            },
+            highlight = {italic = true},
+            ignore_blank_lines = true,
+        }
     end},
 }
