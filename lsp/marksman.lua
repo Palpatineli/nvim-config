97b647055a9fe4669019c787cdd0f3ac24dd801d
Keji Li <mail@keji.li>
2026-07-01 10:36:04 -0400

    fix: more fix for marksman

--- lsp/marksman.lua
+++ lsp/marksman.lua
@@ -4,13 +4,10 @@ local cmd = { bin_name, 'server' }
 local current_file = vim.api.nvim_buf_get_name(0)
 local root_match = vim.fs.find({'.marksman.toml', '.git'}, {path=current_file, upward=true})[1]
 local resolved_root = root_match and vim.fs.dirname(root_match) or vim.uv.cwd()
+resolved_root = resolved_root:gsub("\\", "/")
 
 return {
-    capabilities = vim.tbl_deep_extend(
-        'force',
-        vim.lsp.protocol.make_client_capabilities(),
-        require('blink.cmp').get_lsp_capabilities()
-    ),
+    capabilities = require('blink.cmp').get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities()),
     cmd = cmd,
     filetypes = { 'markdown', 'markdown.mdx' },
     root_dir = resolved_root,
