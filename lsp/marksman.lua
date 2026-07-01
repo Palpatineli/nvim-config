aa73861e122766c22d6901291f72d0072c2eecd4
Keji Li <mail@keji.li>
2026-07-01 10:25:42 -0400

    fix: manual root finding attempt to solve marksman issue in windows

--- lsp/marksman.lua
+++ lsp/marksman.lua
@@ -1,9 +1,18 @@
 local bin_name = 'marksman'
 local cmd = { bin_name, 'server' }
 
+local current_file = vim.api.nvim_buf_get_name(0)
+local root_match = vim.fs.find({'.marksman.toml', '.git'}, {path=current_file, upward=true})[1]
+local resolved_root = root_match and vim.fs.dirname(root_match) or vim.uv.cwd()
+
 return {
-    capabilities = require'blink.cmp'.get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    capabilities = vim.tbl_deep_extend(
+        'force',
+        vim.lsp.protocol.make_client_capabilities(),
+        require('blink.cmp').get_lsp_capabilities()
+    ),
     cmd = cmd,
     filetypes = { 'markdown', 'markdown.mdx' },
-    root_markers = { '.marksman.toml', '.git' },
+    root_dir = resolved_root,
+    root_markers = {'.marksman.toml', '.git'},
 }
